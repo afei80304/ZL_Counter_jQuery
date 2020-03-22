84984c02b9b78204bf608998d8ee78fee45bf20d
afei80304 <afei80304@gmail.com>
2020-03-22 11:26:51 +0800

    完成計時器

--- counter.js
+++ counter.js
@@ -1,17 +1,28 @@
-//each 執行每一個陣列的內容
-//attr 取得屬性
+$(window).scroll(function () {
+    console.log("捲動中");
 
-$("*").each(function(){
-    var number = $(this).attr("data-ct-number");
-    var dur = $(this).attr("data-ct-duration");
+    //each 執行每一個陣列的內容
+    //attr 取得屬性
+    $("*").each(function () {
 
-    $(this).animate({
-        countNum:number
-    }, {
-        duration: dur,
-        easing: "linear",
-        step: function(now){
-            $(this).text(Math.floor(now))
+        var windowtop = $(window).scrollTop();//視窗高度
+        var offset = $(this).offset();//元素高度
+        var top = offset.top;
+        console.log("目前元素的高度: " + top)
+
+        if (windowtop > top - 300) {
+            var number = $(this).attr("data-ct-number");
+            var dur = $(this).attr("data-ct-duration");
+            $(this).animate({
+                countNum: number
+            }, {
+                    // 時間:將字串轉成整數
+                    duration: parseInt(dur),
+                    easing: "linear",
+                    step: function (now) {
+                        $(this).text(Math.floor(now))
+                    }
+                });
         }
     });
-})
+});
